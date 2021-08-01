857bd72f2e4f544a3076763105b11fa1c9741961
Humberto <beto.gonzalez.vela@gmail.com>
2021-08-01 18:44:02 -0500

    Se agregaron archivos raros, mejor crearé otra branch jaja

---
+++ app/src/main/java/com/example/myfirstphpapp/ubicacion.kt
@@ -0,0 +1,11 @@
+package com.example.myfirstphpapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ubicacion : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ubicacion)
+    }
+}
